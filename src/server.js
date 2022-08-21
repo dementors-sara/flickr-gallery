e1e23f8b43c12dfcb1bc3c7b6ddd589f948a63ad
Sara Bäckman <saraba@axis.com>
2022-08-21 11:27:17 +0200

    add endpoints for listing gallery and photo info

--- src/server.js
+++ src/server.js
@@ -1,9 +1,46 @@
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv');
+const axios = require('axios');
 
+dotenv.config();
+const app = express();
 let port = 3000;
 
 app.use(express.static('dist'));
+
+app.use('/lookup-gallery', async (req, res) => {
+  const data = {
+    method: 'flickr.urls.lookupGallery',
+    url: 'https://www.flickr.com/photos/196317236@N03/galleries/72157720979920508/',
+    api_key: process.env.FLICKR_API_KEY,
+  };
+
+  const parameters = new URLSearchParams(data);
+  const url = `https://api.flickr.com/services/rest/?${parameters}`;
+  try {
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    console.log('ERROR', err);
+  }
+});
+
+app.use('/get-photos', async (req, res, next) => {
+  const data = {
+    method: 'flickr.galleries.getPhotos',
+    gallery_id: '196294182-72157720979920508',
+    api_key: process.env.FLICKR_API_KEY,
+  };
+
+  const parameters = new URLSearchParams(data);
+  const url = `https://api.flickr.com/services/rest/?${parameters}`;
+  try {
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    console.log('ERROR', err);
+  }
+});
 
 app.listen(port, function () {
   console.log('Server listening on port %s', port);
